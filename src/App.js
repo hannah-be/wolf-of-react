e60b249fefa776c740198ae52ed05162fa8375a1
Hannah Bourke <hannah25@gmail.com>
2018-01-05 12:15:10 +1100

    Add stockinfo component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './piggy-bank.png';
 import './App.css';
+import StockInfo from './components/StockInfo'
 
 class App extends Component {
   render() {
@@ -9,12 +10,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Share Prices</h1>
         </header>
+        <StockInfo 
+            symbol="NFLX"
+            companyName="Netflix Inc."
+            primaryExchange="Nasdaq Global Select"
+            latestPrice={188.15}
+            latestSource='Close'
+            week52High={204.38}
+            week52Low={113.95}
+        />
+
         <small>
-          <p>Icons made by <a href="http://www.freepik.com" title="Freepik">
+          <p>Icon made by <a href="http://www.freepik.com" title="Freepik">
             Freepik
           </a> from <a href="https://www.flaticon.com/" title="Flaticon">
             www.flaticon.com
-          </a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">
+          </a>. Licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">
             CC 3.0 BY
           </a></p>
         </small>
