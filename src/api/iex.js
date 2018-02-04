fdf08bb06a0000076546dd30dcf303da3e16fba5
Hannah Bourke <hannah25@gmail.com>
2018-02-04 13:01:47 +1100

    Additional api calls

--- src/api/iex.js
+++ src/api/iex.js
@@ -5,10 +5,25 @@ const api = axios.create({
 });
 
 // Wrap .get in a function so it doesn't execute immediately on loading
-export function loadQuoteForStock(symbol) {
+export const loadQuoteForStock = (symbol) => {
   return api.get(`${symbol}/quote`)
     .then(res => {
 //this is like going down a level because what we get returned is one level above what we wanted, ie, this gives us the json data instead of an object comprising that data
       return res.data;
     });
+}
+
+export const loadQuoteForStockLogo = (symbol) => {
+  return api.get(`${symbol}/logo`)
+    .then(res => res.data);
+}
+
+export const loadRecentNewsForStock = (symbol) => {
+  return api.get(`${symbol}/news`)
+    .then(res => res.data)
+}
+
+export const loadChartForStock = (symbol, range) => {
+  return api.get(`${symbol}/chart/${range}`)
+    .then(res => res.data)
 }
